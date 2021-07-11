4c8122d5bf3e0376846859ce2d803f6badc136e0
naima-shk <naimashaikh550@gmail.com>
2021-07-11 11:42:39 +0500

    'product-app-in-progress'

---
+++ app.js
@@ -0,0 +1,19 @@
+function myItems(){
+    let Products= [{
+        productName :'Nike shoes',
+        image:,
+        tag: 'footwear'
+    },
+    {
+        productName :'Nike shoes',
+        image:,
+        tag: 'footwear'
+    },
+    {
+    productName :'Nike shoes',
+        image:,
+        tag: 'footwear'
+    }
+
+]
+}

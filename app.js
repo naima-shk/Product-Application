13078bc7ce8c47ddf2aa4e6854ce77f7d71711d1
naima-shk <naimashaikh550@gmail.com>
2021-07-14 21:43:46 +0500

    'Implemented-Product-App'

--- app.js
+++ app.js
@@ -24,31 +24,30 @@ function myItems() {
     }
 
 ]*/
-let list =document.querySelector('.list').children;
+let list = document.querySelector(".list").children;
 
-let main =document.querySelector('.items').children;
-for (let i=0; i<list.length; i++){
-    list[i].onclick=function(){
-        for (let x=0; x<list.length; x++){
-         list[x].classList.remove('active');
-        }
-        this.classList.add('active');
-        const displayItems = this.getAttribute('data-filter');
-        for (let z=0; z<list.length; z++){
-            main[z].style.transform = 'scale(0)';
-            setTimeout(() =>
-            {
-                main[z].style.display = 'none';
-            }, 500);
-            if ((main[z].getAttribute('data-category') == displayItems) || displayItems == 'all')
-            {
-                main[z].style.transform = 'scale(1)';
-                setTimeout(() =>
-                {
-                    main[z].style.display = 'block';
-                }, 500);
-            }
-        }
+let main = document.querySelector(".items").children;
+for (let i = 0; i < list.length; i++) {
+  list[i].onclick = function () {
+    for (let x = 0; x < list.length; x++) {
+      list[x].classList.remove("active");
     }
+    this.classList.add("active");
+    const displayItems = this.getAttribute("data-filter");
+    for (let z = 0; z < list.length; z++) {
+      main[z].style.transform = "scale(0)";
+      setTimeout(() => {
+        main[z].style.display = "none";
+      }, 500);
+      if (
+        main[z].getAttribute("data-category") == displayItems ||
+        displayItems == "all"
+      ) {
+        main[z].style.transform = "scale(1)";
+        setTimeout(() => {
+          main[z].style.display = "block";
+        }, 500);
+      }
+    }
+  };
 }
-
